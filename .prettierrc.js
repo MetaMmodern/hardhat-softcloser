9a19f15ce21c736332662ddf09d271c677ad2b65
MetaMmodern <shabanovbunya@gmail.com>
2024-06-18 00:25:48 +0300

    make mocha work along with eslint and prettier in cjs env

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-export default {
-  importOrder: ["<THIRD_PARTY_MODULES>", "^[./]"],
-importOrderParserPlugins: ["typescript"],
-importOrderSeparation: true,
-importOrderSortSpecifiers: true,
-"plugins": ["@trivago/prettier-plugin-sort-imports"],
-}
